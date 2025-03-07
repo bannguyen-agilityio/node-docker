d8a8e6b51734d88159fb925ab786c525ce08fc29
duong.pham <duong.pham@asnet.com.vn>
2025-03-07 14:49:14 +0700

    feat: implement Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,25 @@
-const Button = () => (
-  <button
-    type='button'
-    className='me-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-  >
-    Default
-  </button>
-);
+export interface ButtonProps {
+  isDisable?: boolean;
+  text: string;
+  onClick?: () => void;
+  icon?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ isDisable, text, onClick, icon }) => {
+  return (
+    <button
+      className={`flex items-center gap-2 rounded-lg px-4 py-2 text-white transition ${
+        isDisable
+          ? 'cursor-not-allowed bg-gray-400'
+          : 'cursor-pointer bg-blue-600 hover:bg-blue-700'
+      }`}
+      onClick={!isDisable ? onClick : undefined}
+      disabled={isDisable}
+    >
+      {icon && <span>{icon}</span>}
+      {text}
+    </button>
+  );
+};
 
 export default Button;
