f6ccff954c72747ada5cd5b0a0920c3fc720f68b
duong.pham <duong.pham@asnet.com.vn>
2025-03-07 15:18:37 +0700

    update: add width for Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ export interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ isDisable, text, onClick, icon }) => {
   return (
     <button
-      className={`flex items-center gap-2 rounded-lg px-4 py-2 text-white transition ${
+      className={`flex w-full items-center justify-center gap-2 rounded-lg px-4 py-2 text-white transition ${
         isDisable
           ? 'cursor-not-allowed bg-gray-400'
           : 'cursor-pointer bg-blue-600 hover:bg-blue-700'
