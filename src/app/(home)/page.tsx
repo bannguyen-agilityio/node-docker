b8f934b0a681263038c047f4ee96f7df50cea1a2
duong.pham <duong.pham@asnet.com.vn>
2025-03-11 15:45:32 +0700

    feat: implement UI for sidebar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,7 @@
-'use client';
-
-import { useRef } from 'react';
-import { Button, Status, TextField } from '@/components';
-import { GearIcon } from '@radix-ui/react-icons';
-import { StatusType } from '@/constants';
-
 export default function Home() {
-  const ref = useRef(null);
-
   return (
     <>
-      <div>Home</div>
-      <TextField ref={ref} label='Password' id='password' />
-      <Button text='Login' />
-      <Button text='Login' isDisable />
-      <Button text='With icon' icon={<GearIcon />} />
-      <div
-        style={{
-          display: 'flex',
-          width: '100%',
-          justifyContent: 'space-between',
-          maxWidth: '800px',
-          margin: '0 auto',
-        }}
-      >
-        <Status status={StatusType.ACTIVE} />
-        <Status status={StatusType.BANNED} />
-        <Status status={StatusType.OFFLINE} />
-        <Status status={StatusType.FAILED} />
-        <Status status={StatusType.ONLINE} />
-        <Status status={StatusType.ENGAGEMENT} />
-      </div>
+      <div className='h-full'>Home page</div>
     </>
   );
 }
